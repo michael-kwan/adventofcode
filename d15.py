1b2c7285ad63d824a7333593a6e4d7843bc048ca
Michael-Kwan <michaelburps@gmail.com>
2016-12-14 21:58:28 -0800

    day15
    part1

---
+++ d15.py
@@ -0,0 +1,14 @@
+def d1(t): return (t+1)% 13 == 0
+def d2(t): return (t+10) % 19 == 0
+def d3(t): return (t+2)% 3 == 0
+def d4(t): return (t+1)% 7 == 0
+def d5(t): return (t+3)% 5 == 0
+def d6(t): return (t+5)% 17 == 0
+
+part1 = 0
+while True:
+    if d1(part1+1) and d2(part1+2) and d3(part1+3) and d4(part1+4) and d5(part1+5) and d6(part1+6):
+        print(part1)
+        break
+    part1 += 1
+
